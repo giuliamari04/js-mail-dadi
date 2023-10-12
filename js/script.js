3cf63afadeb2241ef7ba66b26a5d2cfae47b891f
giuliamari04 <giuliamari04@gmail.com>
2023-10-12 17:18:44 +0200

    esercizio email funzionante

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const utenti = [
+    'pippo@gmail.com',
+    'pluto@gmail.com',
+    'paperino@gmail.com',
+    'topolino@gmail.com'
+];
+
+const button = document.getElementById('bottone');
+const alert = document.querySelector('.alert');
+
+button.addEventListener('click', function () {
+    let email = document.getElementById('email').value;
+    console.log(email);
+    let trovato = false;
+
+    for (let i = 0; i < utenti.length; i++) {
+        let utenteCorrente = utenti[i];
+        if (utenteCorrente === email) {
+            trovato = true;
+        }
+    }
+    let text;
+    if (trovato) {
+        text = 'utente presente ';
+    } else {
+        text = 'non sei nella lista ';
+    }
+    
+   alert.textContent = text;
+    alert.classList.remove('d-none');
+})
